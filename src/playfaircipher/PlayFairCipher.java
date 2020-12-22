b9ac37fb4c2ddcc0dddbaa949790d941339313a9
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2020-12-22 15:16:42 -0300

    Add bigramic block divider

--- src/playfaircipher/PlayFairCipher.java
+++ src/playfaircipher/PlayFairCipher.java
@@ -2,10 +2,32 @@
 
 package playfaircipher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayFairCipher {
     
     public static void main(String[] args) {
+        String plainText = "passaro";
+        List<String> bigramicBlock = new ArrayList<>();
+        int aux = 0;
+        if (plainText.length() % 2 != 0) {
+            plainText += 'x';
+        }
+        for (int i = 0; i < plainText.length();) {
+            String first = String.valueOf(plainText.charAt(i));
+            i++;
+            String second = String.valueOf(plainText.charAt(i));
+            if (first.equals(second)) {
+                first = "";
+                first += 'z';
+            }
+            bigramicBlock.add(first + second);
+            i++;
+        }
         
+        System.out.println(bigramicBlock);
+        System.out.println(bigramicBlock.get(0));
     }
     
 }
