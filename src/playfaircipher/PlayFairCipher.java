f503eb0fc0774519c05e7ad041ca0380d6fa1ed3
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2020-12-27 22:33:54 -0300

    Add plain text encryption

--- src/playfaircipher/PlayFairCipher.java
+++ src/playfaircipher/PlayFairCipher.java
@@ -45,12 +45,78 @@ public class PlayFairCipher {
                 aux++;
             }
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(cipherAlphabetMatrix[i][j]);
+//        for (int i = 0; i < 5; i++) { //print
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print(cipherAlphabetMatrix[i][j]);
+//            }
+//            System.out.println("");
+//        }
+        
+        List encryptedText = new ArrayList();
+        for (int i = 0; i < bigramicBlock.size(); i++) {
+            int a = 0, b = 0, c = 0, d = 0;
+            for (int j = 0; j < 2; j++) {
+                if (j == 0) {
+                    for (int k = 0; k < 5; k++) {
+                        for (int l = 0; l < 5; l++) {
+                            if (cipherAlphabetMatrix[k][l] == bigramicBlock.get(i).charAt(j)) {
+                                a = k;
+                                b = l;
+                            }
+                        }
+                    }
+                }
+                if (j == 1) {
+                    for (int k = 0; k < 5; k++) {
+                        for (int l = 0; l < 5; l++) {
+                            if (cipherAlphabetMatrix[k][l] == bigramicBlock.get(i).charAt(j)) {
+                                c = k;
+                                d = l;
+                                
+                                if (a != c && b != d) {
+                                    encryptedText.add(cipherAlphabetMatrix[a][d]);
+                                    encryptedText.add(cipherAlphabetMatrix[c][b]);
+                                }
+                                if (a == c) {
+                                    if (b == 4) {
+                                        b = 0;
+                                        encryptedText.add(cipherAlphabetMatrix[a][b]);
+                                        encryptedText.add(cipherAlphabetMatrix[c][d+1]);
+                                    } else if (d == 4) {
+                                        d = 0;
+                                        encryptedText.add(cipherAlphabetMatrix[a][b+1]);
+                                        encryptedText.add(cipherAlphabetMatrix[c][d]);
+                                    } else {
+                                        encryptedText.add(cipherAlphabetMatrix[a][b+1]);
+                                        encryptedText.add(cipherAlphabetMatrix[c][d+1]);
+                                    }
+                                }
+                                if (b == d) {
+                                    if (a == 4) {
+                                        a = 0;
+                                        encryptedText.add(cipherAlphabetMatrix[a][b]);
+                                        encryptedText.add(cipherAlphabetMatrix[c+1][d]);
+                                    } else if (c == 4) {
+                                        c = 0;
+                                        encryptedText.add(cipherAlphabetMatrix[a+1][b]);
+                                        encryptedText.add(cipherAlphabetMatrix[c][d]);
+                                    } else {
+                                        encryptedText.add(cipherAlphabetMatrix[a+1][b]);
+                                        encryptedText.add(cipherAlphabetMatrix[c+1][d]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            System.out.println("");
         }
+        
+        String encryptedPlainText = "";
+        for (int i = 0; i < encryptedText.size(); i++) {
+            encryptedPlainText += encryptedText.get(i);
+        }
+        System.out.println("Encrypted plaint text: " + encryptedPlainText);
     }
 
 }
